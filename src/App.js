eef248cd72ddba526f2f9f4b3932a75cec26e1c4
Tihor Nezic <tihor.nezic@gmail.com>
2021-01-30 16:42:40 +0100

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
+import Header from './components/ui/Header'
 
 function App() {
+  const[characters, setCharacters] = useState([])
+  // after it is fetcher (loaded) then it is set to false
+  const[isLoading, setIsLoading] = useState(true)
+
+  // to make the request
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+      console.log(result.data)
+      setCharacters(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
     </div>
   );
 }
